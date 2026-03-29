87b8c729c2646a27f7fc72081e4e296d485e80c9
Naria-286 <188539756+Naria-286@users.noreply.github.com>
2026-03-28 22:31:48 -0600

    js formatting

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,7 +1,5 @@
-/********* create variables *********/
-// useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
-// Do any of these variables need to be initialized when the page is loaded? 
-// When do they need to be reset or updated?
+'use strict'
+
 const FULL_DAY_COST = 35
 const HALF_DAY_COST = 20
 const dayElements = document.querySelectorAll('li')
@@ -13,11 +11,7 @@ const fullBtn = document.getElementById('full')
 let daysAdded = 0
 let totalCost = 0
 let dailyRate = FULL_DAY_COST
-
 /********* colour change days of week *********/
-// when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
-// added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
-
 dayElements.forEach((e) => {
   e.addEventListener('click', () => {
     if (!e.classList.contains("clicked")) {
@@ -28,11 +22,7 @@ dayElements.forEach((e) => {
   })
 })
 
-
-
 /********* clear days *********/
-// when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
-
 clearBtn.addEventListener('click', () => {
   daysAdded = 0
   totalCost = 0
@@ -42,19 +32,13 @@ clearBtn.addEventListener('click', () => {
   calculateCost()
 })
 
-
 /********* change rate *********/
-// when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 halfBtn.addEventListener('click', () => {
   dailyRate = HALF_DAY_COST
   halfBtn.classList.add('clicked')
   fullBtn.classList.remove('clicked')
   calculateCost()
 })
-
-
-
-// when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 
 fullBtn.addEventListener('click', () => {
   dailyRate = FULL_DAY_COST
@@ -63,11 +47,7 @@ fullBtn.addEventListener('click', () => {
   calculateCost()
 })
 
-
-
-
 /********* calculate *********/
-// when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 function calculateCost() {
   costDisplayElt.textContent = daysAdded * dailyRate
 }
--- scripts/contact.js
+++ scripts/contact.js
@@ -1,10 +1,8 @@
-// when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
+'use strict'
 submitBtn = document.getElementById("submit-button")
 submitBtn.addEventListener('click', () => {
   
   const contactMainElt = document.getElementById("contact-page")
   contactMainElt.innerHTML = '<p>Thank you for your message.</p>'
   contactMainElt.classList.add("large-text")
-})
-// hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
-
+})  
