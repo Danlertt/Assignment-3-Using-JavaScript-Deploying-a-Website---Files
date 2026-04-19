8b74c9f67607bb9c148e277d6a0064fa2d7c1678
Danlertt <188539756+Danlertt@users.noreply.github.com>
2026-04-19 08:14:08 -0600

    Add semicolons to booking.js

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,52 +1,52 @@
-const FULL_DAY_COST = 35
-const HALF_DAY_COST = 20
-const dayElements = document.querySelectorAll('li')
-const clearBtn = document.getElementById('clear-button')
-const costDisplayElt = document.getElementById('calculated-cost')
-const halfBtn = document.getElementById('half')
-const fullBtn = document.getElementById('full')
+const FULL_DAY_COST = 35;
+const HALF_DAY_COST = 20;
+const dayElements = document.querySelectorAll('li');
+const clearBtn = document.getElementById('clear-button');
+const costDisplayElt = document.getElementById('calculated-cost');
+const halfBtn = document.getElementById('half');
+const fullBtn = document.getElementById('full');
 
-let daysAdded = 0
-let totalCost = 0
-let dailyRate = FULL_DAY_COST
+let daysAdded = 0;
+let totalCost = 0;
+let dailyRate = FULL_DAY_COST;
 /********* colour change days of week *********/
 dayElements.forEach((e) => {
   e.addEventListener('click', () => {
     if (!e.classList.contains("clicked")) {
-      e.classList.add("clicked")
-      daysAdded += 1
+      e.classList.add("clicked");
+      daysAdded += 1;
     }
-    calculateCost()
-  })
-})
+    calculateCost();
+  });
+});
 
 /********* clear days *********/
 clearBtn.addEventListener('click', () => {
-  daysAdded = 0
-  totalCost = 0
+  daysAdded = 0;
+  totalCost = 0;
   dayElements.forEach((e) => {
-    e.classList.remove('clicked')
-  })
-  calculateCost()
-})
+    e.classList.remove('clicked');
+  });
+  calculateCost();
+});
 
 /********* change rate *********/
 halfBtn.addEventListener('click', () => {
-  dailyRate = HALF_DAY_COST
-  halfBtn.classList.add('clicked')
-  fullBtn.classList.remove('clicked')
-  calculateCost()
-})
+  dailyRate = HALF_DAY_COST;
+  halfBtn.classList.add('clicked');
+  fullBtn.classList.remove('clicked');
+  calculateCost();
+});
 
 fullBtn.addEventListener('click', () => {
-  dailyRate = FULL_DAY_COST
-  fullBtn.classList.add('clicked')
-  halfBtn.classList.remove('clicked')
-  calculateCost()
-})
+  dailyRate = FULL_DAY_COST;
+  fullBtn.classList.add('clicked');
+  halfBtn.classList.remove('clicked');
+  calculateCost();
+});
 
 /********* calculate *********/
 function calculateCost() {
-  costDisplayElt.textContent = daysAdded * dailyRate
+  costDisplayElt.textContent = daysAdded * dailyRate;
 }
 
