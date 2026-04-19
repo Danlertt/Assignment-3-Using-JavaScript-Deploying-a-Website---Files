1c424e472bb929c2a2768815f85b26da8fd2584a
Danlertt <188539756+Danlertt@users.noreply.github.com>
2026-04-19 08:08:44 -0600

    Fix formatting for contact.js and css

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,7 +1,6 @@
-const submitBtn = document.getElementById("submit-button")
+const submitBtn = document.getElementById("submit-button");
 submitBtn.addEventListener('click', () => {
-  
-  const contactMainElt = document.getElementById("contact-page")
-  contactMainElt.innerHTML = '<p>Thank you for your message.</p>'
-  contactMainElt.classList.add("large-text")
-})  
+  const contactMainElt = document.getElementById("contact-page");
+  contactMainElt.innerHTML = '<p>Thank you for your message.</p>';
+  contactMainElt.classList.add("large-text");
+});
